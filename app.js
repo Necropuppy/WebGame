73ab8ab21624011aec3c7762412c898297d0d775
Arda Pekis <github@ardapekis.com>
2017-02-25 15:25:51 -0500

    Add comments

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
+// Copyright 2017 Nathan Foote and Arda Pekis
+
+// Mongo DB imports and setup
 var mongojs = require("mongojs");
 var db = mongojs('Public:secret@localhost:27017/myGame', ['account','progress']);
 
+// Express imports and setup
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+
+// Loads server-specific config file. config.json should NOT be in the git repo
 var config = require('./config.json');
 
+// Serves the client folder to the client.
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client',express.static(__dirname + '/client'));
 
+// Activates the server on the port in config.json
 serv.listen(config.port);
 console.log("Server started.");
 
+// List of connected clients
 var SOCKET_LIST = {};
 
+// The base for all entities on the game map.
 var Entity = function(){
 	var self = {
 		x:250,
@@ -37,6 +47,8 @@ var Entity = function(){
 	return self;
 }
 
+/******** PLAYER DEFINITION AND UTILITIES *******/
+// The base for all player entities on the game map.
 var Player = function(id, username){
 	var self = Entity();
 	self.id = id;
@@ -62,12 +74,15 @@ var Player = function(id, username){
 			self.shootBullet(self.mouseAngle);
 		}
 	}
+
+	// Launches a new bullet from the player at the specified angle
 	self.shootBullet = function(angle){
 		var b = Bullet(self.id,angle);
 		b.x = self.x;
 		b.y = self.y;
 	}
 
+	// Changes the player's speed based on their controls
 	self.updateSpd = function(){
 		if(self.pressingRight)
 			self.spdX = self.maxSpd;
@@ -84,6 +99,7 @@ var Player = function(id, username){
 			self.spdY = 0;
 	}
 
+	// Serializes all of the state information of this player.
 	self.getInitPack = function(){
 		return {
 			id:self.id,
@@ -96,6 +112,8 @@ var Player = function(id, username){
 			score:self.score,
 		};
 	}
+
+	// Serializes player data that changes over time.
 	self.getUpdatePack = function(){
 		return {
 			id:self.id,
@@ -107,14 +125,22 @@ var Player = function(id, username){
 		}
 	}
 
+	// Adds this new player to the global player list
 	Player.list[id] = self;
 
+	// Add this player to the global init pack.
 	initPack.player.push(self.getInitPack());
 	return self;
 }
+
+// List of all players currently in the game.
 Player.list = {};
+
+// Handles the initial connection of a player.
 Player.onConnect = function(socket, username){
 	var player = Player(socket.id, username);
+
+	// Handles the input controls for the player.
 	socket.on('keyPress',function(data){
 		if(data.inputId === 'left')
 			player.pressingLeft = data.state;
@@ -130,12 +156,15 @@ Player.onConnect = function(socket, username){
 			player.mouseAngle = data.state;
 	});
 
+	// Sends the init pack to the user with the entire game state.
 	socket.emit('init',{
 		selfId:socket.id,
 		player:Player.getAllInitPack(),
 		bullet:Bullet.getAllInitPack(),
 	})
 }
+
+// All state information for players in the game.
 Player.getAllInitPack = function(){
 	var players = [];
 	for(var i in Player.list)
@@ -143,10 +172,13 @@ Player.getAllInitPack = function(){
 	return players;
 }
 
+// Handles the disconnection of a player.
 Player.onDisconnect = function(socket){
 	delete Player.list[socket.id];
 	removePack.player.push(socket.id);
 }
+
+// Incrementally updates all players' game state information.
 Player.update = function(){
 	var pack = [];
 	for(var i in Player.list){
@@ -157,7 +189,8 @@ Player.update = function(){
 	return pack;
 }
 
-
+/******** BULLET DEFINITION AND UTILITIES ********/
+// Bullet entity for projectiles that collide and cause damage.
 var Bullet = function(parent,angle){
 	var self = Entity();
 	self.id = Math.random();
@@ -168,19 +201,24 @@ var Bullet = function(parent,angle){
 	self.toRemove = false;
 	var super_update = self.update;
 	self.update = function(){
+		// Kills the bullet after 100 frames
 		if(self.timer++ > 100)
 			self.toRemove = true;
 		super_update();
 
+		// Collision detection loop
 		for(var i in Player.list){
 			var p = Player.list[i];
+			// Check if the bullet is within a radius of 32 pixels of a player
 			if(self.getDistance(p) < 32 && self.parent !== p.id){
 				p.hp -= 1;
-
+				// Check if the player just hit is dead.
 				if(p.hp <= 0){
 					var shooter = Player.list[self.parent];
+					// Increment the score of the murderer, if one exists.
 					if(shooter)
 						shooter.score += 1;
+					// Respawns the player.
 					p.hp = p.hpMax;
 					p.x = Math.random() * 500;
 					p.y = Math.random() * 500;
@@ -189,6 +227,8 @@ var Bullet = function(parent,angle){
 			}
 		}
 	}
+
+	// Returns all of the state information for the bullet.
 	self.getInitPack = function(){
 		return {
 			id:self.id,
@@ -196,6 +236,7 @@ var Bullet = function(parent,angle){
 			y:self.y,
 		};
 	}
+	// Returns all state information that has changed.
 	self.getUpdatePack = function(){
 		return {
 			id:self.id,
@@ -204,17 +245,21 @@ var Bullet = function(parent,angle){
 		};
 	}
 
+	// Adds the new bullet to the global lists.
 	Bullet.list[self.id] = self;
 	initPack.bullet.push(self.getInitPack());
 	return self;
 }
+// Global bullet list
 Bullet.list = {};
 
+// Updates all of the bullets in the game.
 Bullet.update = function(){
 	var pack = [];
 	for(var i in Bullet.list){
 		var bullet = Bullet.list[i];
 		bullet.update();
+		// Add bullet update to the appropriate global data pack.
 		if(bullet.toRemove){
 			delete Bullet.list[i];
 			removePack.bullet.push(bullet.id);
@@ -224,6 +269,7 @@ Bullet.update = function(){
 	return pack;
 }
 
+// Gets all the state information for bullets.
 Bullet.getAllInitPack = function(){
 	var bullets = [];
 	for(var i in Bullet.list)
@@ -231,8 +277,12 @@ Bullet.getAllInitPack = function(){
 	return bullets;
 }
 
+
+/******** SERVER CALLBACKS ********/
+// Allows DEBUG commands to be used in chat. DANGER!!!
 var DEBUG = true;
 
+// Check if the client entered a valid password.
 var isValidPassword = function(data,cb){
 	db.account.find({username:data.username,password:data.password},function(err,res){
 		if(res.length > 0)
@@ -241,6 +291,8 @@ var isValidPassword = function(data,cb){
 			cb(false);
 	});
 }
+
+// Check if the client entered an existing username
 var isUsernameTaken = function(data,cb){
 	db.account.find({username:data.username},function(err,res){
 		if(res.length > 0)
@@ -249,20 +301,26 @@ var isUsernameTaken = function(data,cb){
 			cb(false);
 	});
 }
+
+// Register a new username and password into the database.
 var addUser = function(data,cb){
 	db.account.insert({username:data.username,password:data.password},function(err){
 		cb();
 	});
 }
 
+// Import the socket.io library.
 var io = require('socket.io')(serv,{});
+// Establishes data connection to client.
 io.sockets.on('connection', function(socket){
 	socket.id = Math.random();
 	SOCKET_LIST[socket.id] = socket;
 
+	// Handles client sign-ins. 'data' contains the username and password
 	socket.on('signIn',function(data){
 		isValidPassword(data,function(res){
 			if(res){
+				// Log the client in and create a player in the game.
 				Player.onConnect(socket, data.username);
 				socket.emit('signInResponse',{success:true});
 			} else {
@@ -270,6 +328,8 @@ io.sockets.on('connection', function(socket){
 			}
 		});
 	});
+
+	// Handles registration of new players.
 	socket.on('signUp',function(data){
 		isUsernameTaken(data,function(res){
 			if(res){
@@ -282,11 +342,13 @@ io.sockets.on('connection', function(socket){
 		});
 	});
 
-
+	// Removes the client and its player when the client disconnects.
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
 		Player.onDisconnect(socket);
 	});
+
+	// Handles incoming chat messages from the client.
 	socket.on('sendMsgToServer',function(data){
 		var playerName = ("" + Player.list[socket.id].username).slice(0,20);
 		for(var i in SOCKET_LIST){
@@ -294,36 +356,38 @@ io.sockets.on('connection', function(socket){
 		}
 	});
 
+	// Handles incoming DEBUG commands from the client.
 	socket.on('evalServer',function(data){
 		if(!DEBUG)
 			return;
 		var res = eval(data);
 		socket.emit('evalAnswer',res);
 	});
-
-
-
 });
 
+// The global lists of data to send to the clients.
 var initPack = {player:[],bullet:[]};
 var removePack = {player:[],bullet:[]};
 
-
+// The main game loop.
 setInterval(function(){
 	var pack = {
 		player:Player.update(),
 		bullet:Bullet.update(),
 	}
 
+	// Send data packs to all connected clients.
 	for(var i in SOCKET_LIST){
 		var socket = SOCKET_LIST[i];
 		socket.emit('init',initPack);
 		socket.emit('update',pack);
 		socket.emit('remove',removePack);
 	}
+
+	// Reset the data packs to empty, to be filled for the next frame.
 	initPack.player = [];
 	initPack.bullet = [];
 	removePack.player = [];
 	removePack.bullet = [];
-
+// Run at 25 FPS.
 },1000/25);
