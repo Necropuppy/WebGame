556105bd7fecc7df788945021bc6cada3e0ccd22
Arda Pekis <github@ardapekis.com>
2017-03-04 00:07:07 -0500

    Finish map processor

--- ProcessMap.js
+++ ProcessMap.js
@@ -1,4 +1,15 @@
 var tmx = require("tmx-parser");
+var jsfile = require("jsonfile")
 tmx.parseFile('simplemap.tmx', function (err, data) {
-    console.log(data.layers[0]);
+    var tiles = [];
+    var cur = [];
+    for (var i = 0; i < data.layers[0].tiles.length; i++) {
+        if (i % data.width === 0) {
+            if (i !== 0) tiles.push(cur);
+            cur = [];
+        }
+        cur.push(data.layers[0].tiles[i].id);
+    }
+    tiles.push(cur);
+    jsfile.writeFile('./simplemap.json', tiles, function(){});
 });
