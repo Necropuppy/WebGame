903061aa2ef265998939e40a036d088cf77c14de
Nathan Foote <nfoote.alt@gmail.com>
2017-03-04 01:14:21 -0500

    required Map

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var mongojs = require("mongojs");
 var db = mongojs('Public:secret@localhost:27017/myGame', ['account']);
 
 require('./Entity');
-//require('./Map');
+require('./Map');
 
 // Express imports and setup
 var express = require('express');
